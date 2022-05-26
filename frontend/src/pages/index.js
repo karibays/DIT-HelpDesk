f997efb3a54f0e769b2c0bb3c3acbeee115c3992
Alibek <alibek.mombekov@mail.ru>
2022-05-26 13:53:25 +0600

    initialize frontend with pages

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import Categories from "./Categories";
+import Forum from "./Forum";
+import Ticket from "./Ticket";
+import MainPage from "./MainPage";
+import NoPage from "./NoPage";
+
+export { Categories, Forum, Ticket, MainPage, NoPage };
