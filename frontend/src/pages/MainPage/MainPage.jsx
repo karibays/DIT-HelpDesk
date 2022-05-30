bd399775cdde3fd9ebe2f0c4fa057e71192bb5f3
Vladislav <vemendu@gmail.com>
2022-05-30 22:01:26 +0600

    MainPage in progress

---
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import NavBar from "../../components/NavBar";
+import "./MainPage.css";
+
+const MainPage = () => {
+  return (
+    // <div>
+    //   <NavBar />
+    //   Hello World (WORK IN PROGRESS)
+    // </div>
+
+      <div className="mainDiv">
+          <NavBar />
+          <h1 className="mainHeader">Как мы можем вам помочь?</h1>
+          <form className="searchQuestionForm" action="">
+              <input type="text" className="searchQuestion" placeholder="Задайте вопрос"/>
+              <button type="submit" className="searchButton"><i className="fa fa-search"></i></button>
+          </form>
+      </div>
+  );
+};
+
+export default MainPage;
