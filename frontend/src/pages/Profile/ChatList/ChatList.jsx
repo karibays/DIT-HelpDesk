57af92d3d0e2a699b6657828a9e486488be4319f
OneStep55 <vnmcfn@gmail.com>
2022-05-30 15:26:41 +0600

    add get request for problems

--- frontend/src/pages/Profile/ChatList/ChatList.jsx
+++ frontend/src/pages/Profile/ChatList/ChatList.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 const ChatList = () => {
-  return <div>ChatList</div>;
+
+  const [problems, setProblems] = useState([])
+
+  useEffect(() => {
+    //  const getProblems = async () => {
+    //      const res = await axios.get(`http://10.1.11.249:8080/problem/get_all_problems/1`)
+
+
+    //  }
+
+    fetch("http://10.1.11.249:8080/problem/get_all_problems/1")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setProblems(data)
+        console.log(problems)
+      });
+
+
+
+    // getProblems()
+
+  }, [])
+  return <div className="container">
+
+{
+      problems.map(problem => {
+        return (
+          <div key={problem.problemId}>
+          <h2>{problem.title}</h2>
+          <h2>{problem.problemId}</h2>
+          <p>{problem.description}</p>
+         </div>
+        )
+      })
+    }
+  </div>;
 };
 
 export default ChatList;
