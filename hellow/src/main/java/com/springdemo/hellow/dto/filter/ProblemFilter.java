878bd87699092f282329b0bfa43306ce3f56dad1
Daryn <darynzhambyl@mail.ru>
2022-06-07 15:58:00 +0600

    removed date search

--- hellow/src/main/java/com/springdemo/hellow/dto/filter/ProblemFilter.java
+++ hellow/src/main/java/com/springdemo/hellow/dto/filter/ProblemFilter.java
@@ -3,6 +3,5 @@ package com.springdemo.hellow.dto.filter;
 import java.time.LocalDate;
 
 public record ProblemFilter(
-                            LocalDate dateUntil,
                             String title) {
 }
