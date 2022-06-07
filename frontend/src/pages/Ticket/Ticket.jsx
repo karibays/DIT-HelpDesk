c4cd824daee5e74768a8e159c58e9bac782ca5c6
olimeme <alibek.mombekov@mail.ru>
2022-06-07 14:35:43 +0600

    style ticket form

--- frontend/src/pages/Ticket/Ticket.jsx
+++ frontend/src/pages/Ticket/Ticket.jsx
@@ -8,7 +8,7 @@ const Ticket = () => {
   const { id } = fetchUser();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-
+  const [selectedCategory, setSelectedCategory] = useState(1);
   const problemSubmit = async (e) => {
     let form_data = new FormData();
     // form_data.append("title", title);
@@ -20,7 +20,7 @@ const Ticket = () => {
       title,
       description,
       userId: id,
-      categoryId: 1,
+      categoryId: selectedCategory,
     };
 
     axios
@@ -36,12 +36,22 @@ const Ticket = () => {
         console.warn(error);
       });
   };
+
+  const handleSelect = (event) => {
+    setSelectedCategory(event.target.value);
+    console.log(event.target.value);
+  };
   return (
     <div>
       <NavBar lightMode={true} />
-      <div className="container">
+      <div className="ticket-container container">
+        <h2>Заполнить анкету</h2>
+        <h5 className="text-muted">
+          Не забудьте заполните поля более подробней, чтобы упростить работу
+          нашим специалистам!
+        </h5>
         <form onSubmit={problemSubmit}>
-          <h5>Тема</h5>
+          {/* <h5>Тема</h5>
           <input
             className="form"
             type="text"
@@ -59,9 +69,64 @@ const Ticket = () => {
             placeholder="Описание проблемы"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-          />
+          /> */}
 
-          <button type="submit">Отправить</button>
+          <div class="form-group">
+            <label for="exampleSelect1" class="form-label mt-4">
+              Выберите категорию
+            </label>
+            <select
+              class="form-select"
+              id="exampleSelect1"
+              onChange={handleSelect}
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label for="exampleTextarea" className="form-label">
+              Что вы хотели сделать?
+            </label>
+            <textarea
+              className="form-control"
+              id="exampleTextarea"
+              rows="3"
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <label for="exampleTextarea" className="form-label">
+              Что привело к проблеме?
+            </label>
+            <textarea
+              className="form-control"
+              id="exampleTextarea"
+              rows="3"
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <label for="exampleTextarea" className="form-label">
+              Что вы испробовали для решения проблемы?
+            </label>
+            <textarea
+              className="form-control"
+              id="exampleTextarea"
+              rows="3"
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <label for="exampleTextarea" className="form-label">
+              Прикрепите полезные снимки вашей пробелмы
+            </label>
+            <input className="form-control" type="file" id="formFile" />
+          </div>
+
+          <button type="submit" className="btn btn-primary btn-lg">
+            Отправить
+          </button>
         </form>
       </div>
     </div>
