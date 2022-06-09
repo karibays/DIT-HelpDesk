dff3d041181696e4d4522c1753b18105e2453187
olimeme <alibek.mombekov@mail.ru>
2022-06-09 10:13:03 +0600

    new problem tickets come up on top of the list

--- frontend/src/pages/Profile/Problems/Problems.jsx
+++ frontend/src/pages/Profile/Problems/Problems.jsx
@@ -24,36 +24,41 @@ const ChatList = () => {
       <div className="problems-container">
         <h3>Ваши анкеты</h3>
         <div className="problems-items list-group">
-          {problems.map(({ id, title, description, createdAt, category }) => {
-            return (
-              <a
-                href="#"
-                className="problems-item list-group-item list-group-item-action"
-                key={id}
-              >
-                <div className="problems-block">
-                  <div className="problems-content">
-                    <span class="badge bg-success problems-badge">Принято</span>
-                    <h4>{title}</h4>
-                    <h6 className="text-muted">{category?.categoryName}</h6>
-                    <p>{description}</p>
-                    <small className="text-muted">
-                      Создано в {createdAt.slice(0, 10)}
-                    </small>
+          {problems
+            .slice(0)
+            .reverse()
+            .map(({ id, title, description, createdAt, category }) => {
+              return (
+                <a
+                  href="#"
+                  className="problems-item list-group-item list-group-item-action"
+                  key={id}
+                >
+                  <div className="problems-block">
+                    <div className="problems-content">
+                      <span className="badge bg-success problems-badge">
+                        Принято
+                      </span>
+                      <h4>{title}</h4>
+                      <h6 className="text-muted">{category?.categoryName}</h6>
+                      <p>{description}</p>
+                      <small className="text-muted">
+                        Создано в {createdAt.slice(0, 10)}
+                      </small>
+                    </div>
+                    <div className="problems-btns">
+                      <button
+                        type="button"
+                        class="btn btn-outline-danger"
+                        onClick={() => deleteProblem(id)}
+                      >
+                        Удалить
+                      </button>
+                    </div>
                   </div>
-                  <div className="problems-btns">
-                    <button
-                      type="button"
-                      class="btn btn-outline-danger"
-                      onClick={() => deleteProblem(id)}
-                    >
-                      Удалить
-                    </button>
-                  </div>
-                </div>
-              </a>
-            );
-          })}
+                </a>
+              );
+            })}
         </div>
       </div>
     </div>
