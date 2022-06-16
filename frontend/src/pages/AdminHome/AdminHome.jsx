acea42dd12629cd233fc7a375868efdb8a6c4791
olimeme <alibek.mombekov@mail.ru>
2022-06-16 13:03:18 +0600

    status and search bar handle in admin home page

--- frontend/src/pages/AdminHome/AdminHome.jsx
+++ frontend/src/pages/AdminHome/AdminHome.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import NavBar from "../../components/NavBar";
 import Badge from "../Profile/Badge";
 import axios from "axios";
@@ -7,41 +7,81 @@ import { GlobalState } from "../../components/Context/GloblalState";
 import { Link, useNavigate } from "react-router-dom";
 
 const AdminHome = () => {
-  const state = useContext(GlobalState);
+  const { problemsAPI, statusesAPI } = useContext(GlobalState);
   const adminState = true;
   const navigate = useNavigate();
-  const allProblems = state.problemsAPI.allProblems;
-  const [status, setStatus] = state.problemsAPI.status
-  const [statuses, setStatuses] = state.statusesAPI.statuses
+  const [allProblems, setAllProblems] = problemsAPI.allProblems;
+  const [status, setStatus] = problemsAPI.status;
+  const [statuses, setStatuses] = statusesAPI.statuses;
+  const [error, setError] = useState(false);
+  const handleSearch = (str) => {
+    if (str.length > 3)
+      axios
+        .get(`http://10.1.11.249:8080/problems?title=${str}`)
+        .then(({ data: { content } }) => {
+          console.log(content);
+          setAllProblems(content);
+        })
+        .catch(() => setError(true));
+    else
+      axios
+        .get("http://10.1.11.249:8080/problems")
+        .then(({ data }) =>
+          setAllProblems(
+            data.content
+              .sort((prob1, prob2) => prob1.id - prob2.id)
+              .slice(0)
+              .reverse()
+          )
+        )
+        .catch(() => setError(true));
+  };
 
+  const handleStatus = (statusId) => {
+    if (statusId == 0) {
+      axios
+        .get("http://10.1.11.249:8080/problems")
+        .then(({ data }) =>
+          setAllProblems(
+            data.content
+              .sort((prob1, prob2) => prob1.id - prob2.id)
+              .slice(0)
+              .reverse()
+          )
+        )
+        .catch(() => setError(true));
+    } else setStatus(statusId);
+  };
 
-  const handleStatus = e => {
-
-    setStatus(e.target.value)
-  }
+  useEffect(() => {
+    handleStatus(status);
+  }, []);
 
   return (
     <>
       <NavBar admin={true} />
       <div className="container">
-        <div className="form-group">
+        <div className="form-group mt-4">
           <input
-            className="categories-search form-control form-control-lg mt-4"
+            className="categories-search form-control form-control-lg"
             type="text"
             placeholder="Поиск проблемы"
             id="inputLarge"
-          // onChange={handleSearch}
+            onChange={(e) => handleSearch(e.target.value)}
           />
 
-
-          <select name="status" value={status} onChange={handleStatus} class="form-select mt-4" id="exampleSelect1">
-            {
-              statuses.map(obj => (
-                <option value={obj.id} key={obj.id}>
-                  {obj.statusName}
-                </option>
-              ))
-            }
+          <select
+            name="status"
+            onChange={(e) => handleStatus(e.target.value)}
+            className="form-select mt-4 col-2"
+            id="exampleSelect1"
+          >
+            <option value={0}>Не выбрано</option>
+            {statuses.map((obj) => (
+              <option value={obj.id} key={obj.id}>
+                {obj.statusName}
+              </option>
+            ))}
           </select>
 
           <div className="mt-4">
