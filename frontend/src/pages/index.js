191925ce34164c53ae39dc6790b34a066baa5621
karibays <karibay.91@mail.ru>
2022-06-16 14:53:25 +0600

    initial commit

--- frontend/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Categories from "./Categories";
-import Forum from "./Forum";
-import Ticket from "./Ticket";
-import MainPage from "./MainPage";
-import NoPage from "./NoPage";
-
-export { Categories, Forum, Ticket, MainPage, NoPage };
